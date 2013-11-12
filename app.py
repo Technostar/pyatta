0a564e42e8029f9072a383c46866ce2f33eabf76
root <root@vyatta.(none)>
2013-11-12 23:11:21 +0000

    adding vyatta module

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from flask import Flask, request
+import sys
+import subprocess
+from pyparsing import nestedExpr
+from itertools import izip
+import vyatta
+
+def init_env():
+    '''
+    Initialize the environment with some params
+    '''
+    #not yet implemented
+
+app = Flask(__name__)
+
+@app.route('/shutdown', methods = ['POST'])
+def shutdown_server():
+    '''
+    This function is for shutting down the flask web server
+    '''
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    return 'Pyatta API shutting down...'
+
+if __name__ == "__main__":
+    print vyatta.get_interfaces_infos('ovpn')
+    #if sys.argv:
+        #Starting server
+        #if sys.argv[1] == 'start': app.run(host='0.0.0.0', debug = True)
